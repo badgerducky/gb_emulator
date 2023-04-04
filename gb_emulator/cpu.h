2f6f1347e305268a7f9c0ed7a485a01a8927b191
Badger <badgerducky@gmail.com>
2023-04-03 21:35:14 -0400

    add registers and enum for flags

--- gb_emulator/cpu.h
+++ gb_emulator/cpu.h
@@ -2,11 +2,26 @@
 
 #pragma once
 
+#include <stdint.h>
+
 
 class CPU
 {
 private:
     /* data */
+    // Registers
+    uint8_t accumulator = 0b00000000;
+    uint8_t flags_reg = 0b00000000;
+    uint8_t b_register = 0b00000000;
+    uint8_t c_register = 0b00000000;
+    uint8_t e_register = 0b00000000;
+    uint8_t h_register = 0b00000000;
+    uint8_t l_register = 0b00000000;
+
+    uint16_t sp_register = 0b0000000000000000;
+    uint16_t pc_register = 0b0000000000000000;
+    enum FLAGS {Z, N, H, C};
+
 public:
     CPU(/* args */);
     ~CPU();
