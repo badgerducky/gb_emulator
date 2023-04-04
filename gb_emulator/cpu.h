b7a04d592e73d87995f06181501e3d31b74f3e3f
Badger <badgerducky@gmail.com>
2023-04-03 21:41:12 -0400

    add registers and flags as private members of CPU

--- gb_emulator/cpu.h
+++ gb_emulator/cpu.h
@@ -8,18 +8,20 @@
 class CPU
 {
 private:
-    /* data */
     // Registers
     uint8_t accumulator = 0b00000000;
     uint8_t flags_reg = 0b00000000;
     uint8_t b_register = 0b00000000;
     uint8_t c_register = 0b00000000;
+    uint8_t d_register = 0b00000000;
     uint8_t e_register = 0b00000000;
     uint8_t h_register = 0b00000000;
     uint8_t l_register = 0b00000000;
-
     uint16_t sp_register = 0b0000000000000000;
     uint16_t pc_register = 0b0000000000000000;
+    //8 bit registers: a b c d e h l hl (Value at memory location pointed to by hl register)
+    //16 bit registers: b+c d+e hl sp
+    //flags Zero, ?, ?, Carry
     enum FLAGS {Z, N, H, C};
 
 public:
