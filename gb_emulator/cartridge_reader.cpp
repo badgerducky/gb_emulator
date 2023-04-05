669f1e035279a962f64e27dabb1a89203ab1837e
Badger <badgerducky@gmail.com>
2023-04-05 18:34:54 -0400

    add file read

--- gb_emulator/cartridge_reader.cpp
+++ gb_emulator/cartridge_reader.cpp
@@ -8,8 +8,8 @@ using std::endl;
 using std::ios;
 
 struct stat results;
-int get_file_size(){
-    if (stat("input.bin", &results) == 0) {
+void get_file_size_bytes(const char * filename){
+    if (stat(filename, &results) == 0) {
         // The size of the file in bytes is in
         cout << results.st_size << endl;
     }
@@ -19,6 +19,30 @@ int get_file_size(){
     }
 }
 
-char get_bytes(){
-    std::ifstream myFile ("input.bin", ios::in | ios::binary);
+void load_rom(const char * filename){
+    std::cout << filename;
+    std::ifstream rom_file (filename, ios::in | ios::binary);
+    if (rom_file){
+        // get length of file:
+        rom_file.seekg (0, rom_file.end);
+        int64_t length = rom_file.tellg();
+        rom_file.seekg (0, rom_file.beg);
+
+        char * rom = new char [length];
+        std::cout << "Reading " << length << " characters... ";
+
+        // read data as a block:
+        rom_file.read (rom, length);
+
+        if (rom_file)
+        std::cout << "all characters read successfully.";
+        else
+        std::cout << "error: only " << rom_file.gcount() << " could be read";
+        rom_file.close();
+        size_t len = sizeof(rom_file);
+        cout << len << endl;
+        // ...buffer contains the entire file...
+
+        delete[] rom;
+    }
 }
--- gb_emulator/main.cpp
+++ gb_emulator/main.cpp
@@ -1,10 +1,21 @@
 // main.cpp : Defines the entry point for the application.
 
+#include "cartridge_reader.cpp"
 #include <iostream>
+#include <windows.h>
 using std::cout;
 using std::endl;
+
+
+
 int main()
 {
-	cout << "Hello CMake." << endl;
+	// Get current directory on Windows
+	// char buf[256];
+	// GetCurrentDirectory(256, buf);
+	// cout << buf << endl;
+
+	get_file_size_bytes("..\\..\\..\\gb_emulator\\tests\\input.bin");
+	load_rom("..\\..\\..\\gb_emulator\\tests\\input.bin");
 	return 0;
 }
