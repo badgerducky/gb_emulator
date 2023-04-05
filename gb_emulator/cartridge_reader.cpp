8a491be472a402feea6b88ce38dfddd0863f8a3d
Badger <badgerducky@gmail.com>
2023-04-05 17:04:05 -0400

    add ifstream

--- gb_emulator/cartridge_reader.cpp
+++ gb_emulator/cartridge_reader.cpp
@@ -2,17 +2,23 @@
 
 #include <sys/stat.h>
 #include <iostream>
+#include <fstream>
 using std::cout;
 using std::endl;
+using std::ios;
 
 struct stat results;
 int get_file_size(){
-if (stat("input.bin", &results) == 0) {
-    // The size of the file in bytes is in
-    cout << results.st_size << endl;
+    if (stat("input.bin", &results) == 0) {
+        // The size of the file in bytes is in
+        cout << results.st_size << endl;
+    }
+    else {
+        // An error occurred
+        cout << "File not found" << endl;
+    }
 }
-else {
-    // An error occurred
-    cout << "File not found" << endl;
-    }
-    }
+
+char get_bytes(){
+    std::ifstream myFile ("input.bin", ios::in | ios::binary);
+}
