7a26e10fbd98e0b48a3184223a456d8342d01f05
Badger <badgerducky@gmail.com>
2023-04-17 16:57:00 -0400

    new generic path

--- gb_emulator/main.cpp
+++ gb_emulator/main.cpp
@@ -10,12 +10,12 @@ using std::endl;
 
 int main()
 {
-	// Get current directory on Windows
-	// char buf[256];
-	// GetCurrentDirectory(256, buf);
-	// cout << buf << endl;
+	//Get current directory on Windows
+	char buf[256];
+	GetCurrentDirectory(256, buf);
+	cout << buf << endl;
 
-	get_file_size_bytes("..\\..\\..\\gb_emulator\\tests\\input.bin");
-	load_rom("..\\..\\..\\gb_emulator\\tests\\input.bin");
+	get_file_size_bytes("..\\..\\gb_emulator\\gb_emulator\\tests\\input.bin");
+	load_rom("..\\..\\gb_emulator\\gb_emulator\\tests\\input.bin");
 	return 0;
 }
