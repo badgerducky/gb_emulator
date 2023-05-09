3405cf4ab718d26a09640949b4597cd80dc4d0b1
badgerducky <seanmct@vt.edu>
2023-05-09 09:43:31 -0400

    initialize rom then read

--- gb_emulator/cartridge_reader.cpp
+++ gb_emulator/cartridge_reader.cpp
@@ -8,35 +8,31 @@ using std::endl;
 using std::ios;
 
 struct stat results;
-void get_file_size_bytes(const char * filename){
-    if (stat(filename, &results) == 0) {
+int64_t get_file_size_bytes(const char *filename)
+{
+    if (stat(filename, &results) == 0)
+    {
         // The size of the file in bytes is in
         cout << "Size of input file in bytes: " << results.st_size << endl;
-        cout << "File found" << endl << endl;
+        cout << "File found" << endl
+             << endl;
     }
-    else {
+    else
+    {
         // An error occurred
         cout << "File not found" << endl;
     }
+    return (results.st_size);
 }
 
-void load_rom(const char * filename, int64_t * rom_size, unsigned char * rom){
-    std::ifstream rom_file (filename, ios::in | ios::binary);
-    if (rom_file){
-        // get length of file:
-        rom_file.seekg (0, rom_file.end);
-        const int64_t length = rom_file.tellg();
-        rom_file.seekg (0, rom_file.beg);
-
-        // set length to var accessible from main
-        *rom_size = length;
-
-        // Read file with mostly C things
-        rom = new unsigned char [length];
+void load_rom(const char *filename, int64_t *rom_size, unsigned char *rom)
+{
+    std::ifstream rom_file(filename, ios::in | ios::binary);
+    if (rom_file)
+    {
         FILE *ptr;
-        ptr = fopen(filename,"rb");  // r for read, b for binary
-        fread(rom,1,length,ptr); // read all bytes from file into rom
-        // ToDo: Does this need a closer?
-
+        ptr = fopen(filename, "rb");   // r for read, b for binary
+        fread(rom, 1, *rom_size, ptr); // read all bytes from file into rom
+        fclose(ptr);
     }
 }
--- gb_emulator/main.cpp
+++ gb_emulator/main.cpp
@@ -3,29 +3,28 @@
 #include "cartridge_reader.cpp"
 #include "cpu.cpp"
 #include <iostream>
-#include <windows.h>
+#include <filesystem>
 using std::cout;
 using std::endl;
-
-
+using std::filesystem::current_path;
 
 int main()
 {
-	//Get current directory on Windows
-	char buf[256];
-	GetCurrentDirectory(256, buf);
-	cout << "Current Directory: " << buf << endl << endl;
+	// Get current directory
+	cout << "Current Directory: " << current_path() << endl
+		 << endl;
 
 	int64_t rom_size = 0; // store size of rom, after getting passed to load_rom function
-	unsigned char * rom = NULL; //store contents of rom after load_rom function
+	rom_size = get_file_size_bytes("gb_emulator/tests/input.bin");
 
-	get_file_size_bytes("..\\..\\gb_emulator\\gb_emulator\\tests\\input.bin");
-	load_rom("..\\..\\gb_emulator\\gb_emulator\\tests\\input.bin", &rom_size, rom);
+	unsigned char *rom = new unsigned char[rom_size]; // store contents of rom after load_rom function
+	load_rom("gb_emulator/tests/input.bin", &rom_size, rom);
 
 	cout << "rom size: " << rom_size << endl;
 
-	for (int64_t i=0; i < rom_size; i++){
-		//printf("%u ", rom[i]);
+	for (int64_t i = 0; i < rom_size; i++)
+	{
+		// printf("%u ", rom[i]);
 		cout << "byte number " << i << ": " << rom[i] << endl;
 	}
 
