c286e979f5dacd9f4db556a4ab8f45fc270d1818
badgerducky <seanmct@vt.edu>
2023-05-14 11:24:58 -0700

    add a few cpu instructions

--- gb_emulator/cpu.cpp
+++ gb_emulator/cpu.cpp
@@ -14,8 +14,18 @@ void get_instruction(unsigned char *rom, int64_t index)
     case 0x01:
         std::cout << "01" << std::endl;
         break;
+    case 0x02:
+        std::cout << "00" << std::endl;
+        break;
+    case 0x03:
+        std::cout << "01" << std::endl;
+        break;
+    case 0x40 ... 0:
+        std::cout << "LD BC" << std::endl;
+        break;
     case 0xff:
-        std::cout << "FF" << std::endl;
+        std::cout << "RST 38H" << std::endl;
         break;
     }
+    // Todo:
 }
